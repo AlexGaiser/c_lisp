ec1e0af87350b8e78c5a1bb6b4ccef26b0cf4d97
AlexGaiser <alexcgaiser@gmail.com>
2021-12-01 20:04:25 -0500

    add loops examples

---
+++ notes/examples/loops.c
@@ -0,0 +1,14 @@
+int main(int argc, char const *argv[])
+{
+  for (int i = 0; i < 5; i++)
+  {
+    puts('hello world');
+  }
+
+  int j = 10;
+  while (j > 0)
+  {
+    puts(j);
+  }
+  return 0;
+}
