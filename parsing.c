6b0b088672f8d5ab7e720afe7cc596c5b9691715
AlexGaiser <alexcgaiser@gmail.com>
2021-12-02 00:20:05 -0500

    add mpc and cleanup for polish notation

--- parsing.c
+++ parsing.c
@@ -28,6 +28,21 @@ void add_history(char *unused) {}
 
 int main(int argc, char const *argv[])
 {
+  /* Create Some Parsers */
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+            Number, Operator, Expr, Lispy);
   puts("A_Lisp Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
@@ -39,6 +54,6 @@ int main(int argc, char const *argv[])
 
     free(input);
   }
-
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
